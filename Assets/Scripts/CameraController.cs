2d849aa96d35ee052603574a4cd9ff67c94a9d76
Osama Kawish <osamakawish@gmail.com>
2018-01-15 10:07:29 -0500

    Finished Roll-A-Ball tutorial from the Unity website.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offsetPosition;
+	//private Vector3 offsetRotation;
+
+	// Use this for initialization
+	void Start () {
+		offsetPosition = transform.position - player.transform.position;
+		// Change offsetRotation.
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offsetPosition;
+	}
+}
